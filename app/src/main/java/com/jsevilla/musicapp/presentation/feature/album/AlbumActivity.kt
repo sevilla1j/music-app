76df0df68b489f369e3e522a1cc4bf712906df0a
Jose A. Sevilla <jose1.sevilla23@gmail.com>
2020-07-13 11:22:00 -0500

    Vista de los albumes lista

--- app/src/main/java/com/jsevilla/musicapp/presentation/feature/album/AlbumActivity.kt
+++ app/src/main/java/com/jsevilla/musicapp/presentation/feature/album/AlbumActivity.kt
@@ -1,12 +1,86 @@
 package com.jsevilla.musicapp.presentation.feature.album
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.Window
 import com.jsevilla.musicapp.R
+import com.jsevilla.musicapp.presentation.base.BaseActivity
+import com.jsevilla.musicapp.presentation.di.HasComponent
+import com.jsevilla.musicapp.presentation.feature.album.di.AlbumComponent
+import com.jsevilla.musicapp.presentation.feature.album.di.DaggerAlbumComponent
+import kotlinx.android.synthetic.main.activity_album.*
 
-class AlbumActivity : AppCompatActivity() {
+class AlbumActivity : BaseActivity(), HasComponent<AlbumComponent>,
+    AlbumFragment.AlbumFragmentListener {
+
+    private var component: AlbumComponent? = null
+
+    private val albumFragment = AlbumFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_album)
+
+        init(savedInstanceState)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val window = window
+        window.setFormat(PixelFormat.RGBA_8888)
+    }
+
+    override fun init(savedInstanceState: Bundle?) {
+        initializeInjector()
+
+        if (savedInstanceState == null) {
+            addFragment(
+                R.id.fltContainer,
+                albumFragment,
+                true,
+                getString(R.string.txt_album)
+            )
+        }
+    }
+
+    override fun initializeInjector() {
+        this.component = DaggerAlbumComponent.builder()
+            .appComponent(appComponent)
+            .activityModule(activityModule)
+            .build()
+    }
+
+    override fun initControls() {
+        //
+    }
+
+    override fun initEvents() {
+        //
+    }
+
+    override fun getComponent(): AlbumComponent? {
+        return component
+    }
+
+    override fun onBackPressed() {
+        this.navigator.navigateToSearch(this)
+    }
+
+    override fun showLoading() {
+        view_loading.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        view_loading.visibility = View.GONE
+    }
+
+    companion object {
+        fun getCallingIntent(context: Context): Intent {
+            return Intent(context, AlbumActivity::class.java)
+        }
     }
 }
--- app/src/main/java/com/jsevilla/musicapp/presentation/feature/album/AlbumFragment.kt
+++ app/src/main/java/com/jsevilla/musicapp/presentation/feature/album/AlbumFragment.kt
@@ -1,15 +1,50 @@
 package com.jsevilla.musicapp.presentation.feature.album
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.jsevilla.musicapp.R
+import com.jsevilla.musicapp.presentation.base.BaseFragment
+import com.jsevilla.musicapp.presentation.feature.album.di.AlbumComponent
+import javax.inject.Inject
 
-class AlbumFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class AlbumFragment : BaseFragment(), AlbumView {
+
+    @Inject
+    internal lateinit var presenter: AlbumPresenter
+    private var albumFragmentListener: AlbumFragmentListener? = null
+
+    private var btnBack: ImageView? = null
+    private var imgPhoto: ImageView? = null
+    private var txtAlbum: TextView? = null
+    private var txtArtist: TextView? = null
+    private var recyclerViewItem: RecyclerView? = null
+
+    override fun context(): Context? {
+        val activity = activity
+        return activity?.applicationContext
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is AlbumFragmentListener) {
+            albumFragmentListener = context
+        }
+    }
+
+    override fun onViewInjected(savedInstanceState: Bundle?) {
+        super.onViewInjected(savedInstanceState)
+        this.presenter.attachView(this)
+    }
+
+    override fun onInjectView(): Boolean {
+        getComponent(AlbumComponent::class.java).inject(this)
+        return true
     }
 
     override fun onCreateView(
@@ -21,8 +56,34 @@ class AlbumFragment : Fragment() {
         return view
     }
 
-    private fun init(view: View?) {
+    override fun onDetach() {
+        super.onDetach()
+        albumFragmentListener = null
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.destroy()
+    }
+
+    private fun init(view: View?) {
+        btnBack = view?.findViewById(R.id.btnBack)
+        imgPhoto = view?.findViewById(R.id.imgPhoto)
+        txtAlbum = view?.findViewById(R.id.txtAlbum)
+        txtArtist = view?.findViewById(R.id.txtArtist)
+        recyclerViewItem = view?.findViewById(R.id.recyclerViewItem)
+
+        btnBack?.setOnClickListener {
+            albumFragmentListener?.onBackPressed()
+        }
+    }
+
+    internal interface AlbumFragmentListener {
+        fun showLoading()
+
+        fun hideLoading()
+
+        fun onBackPressed()
     }
 
     companion object {
